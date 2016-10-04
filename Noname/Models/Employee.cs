64cc5b04910e4d1681a58b8f606a98ccf4735fde
Eat and sleep <eat.and.sleep.001@gmail.com>
2016-10-04 20:55:39 +0700

    0

---
+++ Noname/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Noname.Models
+{
+    [Table("EMPLOYEE_MST")]
+    public class Employee
+    {
+        [Column("EMP_CODE")]
+        [Required]
+        [StringLength(4, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        public string EmployeeCode;
+    }
+}
